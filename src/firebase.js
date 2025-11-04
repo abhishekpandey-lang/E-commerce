24053e0b4d96e5d1b5db06a7e7b5a090fac7e74a
Abhishek <abhishek.pandey@gounicrew.com>
2025-11-04 11:56:13 +0530

    Fixed Firebase bucket name

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,24 @@
 // src/firebase.js
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // ✅ Firestore import
+import { getFirestore } from "firebase/firestore";
 
-// 👇 apna Firebase config
+// ✅ Correct Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAfzcOOW-mjRum3XvVtvcad_h-YhKcOOl0",
   authDomain: "e-commerce-f5f75.firebaseapp.com",
   projectId: "e-commerce-f5f75",
-  storageBucket: "e-commerce-f5f75.firebasestorage.app",
+  storageBucket: "e-commerce-f5f75.appspot.com", // ✅ Corrected
   messagingSenderId: "498678861712",
   appId: "1:498678861712:web:d8f9e775559e3a171a6d15",
   measurementId: "G-59ZM1REN41"
 };
 
-// ✅ Duplicate initialization se bachne ke liye check karo
+// ✅ Initialization
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 // ✅ Auth + Google
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-
-
-
-// ✅ Sab exports
 export { app, auth, googleProvider };
