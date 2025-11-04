1a5f92a77f803fe75c45814efcbe44436fdbc87e
Abhishek <abhishek.pandey@gounicrew.com>
2025-11-04 17:45:34 +0530

    Added Google Analytics integration mi id

--- src/analytics/ga4.js
+++ src/analytics/ga4.js
@@ -1,20 +1,18 @@
+// analytics/ga4.js
 import ReactGA from "react-ga4";
 
 export function initGA(measurementId) {
   if (!measurementId) {
-    console.warn("⚠️ GA measurement ID not provided.");
+    console.warn("GA measurement ID not provided!");
     return;
   }
-
   ReactGA.initialize(measurementId);
-  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-  console.log("✅ Google Analytics initialized:", measurementId);
 }
 
 export function trackPage(path) {
   ReactGA.send({ hitType: "pageview", page: path });
 }
 
-export function trackEvent({ category = "UX", action, label, value }) {
+export function trackEvent({ category = "User", action, label, value }) {
   ReactGA.event({ category, action, label, value });
 }
